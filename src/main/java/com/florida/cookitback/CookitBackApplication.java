84bb2afa50a7578e5e26b3e149c99a275a9c1f65
Ainhoa Lopez <ainhoalv92@gmail.com>
2024-02-28 17:34:41 +0100

    initial commit

---
+++ src/main/java/com/florida/cookitback/CookitBackApplication.java
@@ -0,0 +1,13 @@
+package com.florida.cookitback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookitBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florida/cookitback/CookitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florida.cookitback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
