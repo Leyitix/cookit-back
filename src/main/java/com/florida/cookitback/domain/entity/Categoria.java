8e84fd7ebfb8a5a0889e00e66b198fe091024eef
Ainhoa Lopez <ainhoalv92@gmail.com>
2024-03-13 16:12:02 +0100

    feat(): Se crean las entidades de categorias y recetas que contienen los campos de cada una de ellas

--- src/main/java/com/florida/cookitback/domain/entity/Categoria.java
+++ src/main/java/com/florida/cookitback/domain/entity/Categoria.java
@@ -1,4 +1,59 @@
 package com.florida.cookitback.domain.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity()
+@Table(name = "categorias")
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categorySequence") // generamos el id
+    private Long id;
+
+    @Column(length = 100, nullable = false) // no puede ser nulo
+    @Size(min = 3, max = 100)
+    private String nombre;
+
+    @Column(length = 2000)
+    private String descripcion;
+
+    @Lob
+    private byte[] imagen;
+
+    public Categoria() {
+        // generamos un constructor vacio
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public byte[] getImagen() {
+        return imagen;
+    }
+
+    public void setImage(byte[] imagen) {
+        this.imagen = imagen;
+    }
 }
--- src/main/java/com/florida/cookitback/domain/entity/Receta.java
+++ src/main/java/com/florida/cookitback/domain/entity/Receta.java
@@ -1,4 +1,149 @@
 package com.florida.cookitback.domain.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "recetas")
 public class Receta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "itemSequence")
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    @Size(min = 3, max = 100)
+    private String nombre;
+
+    @Column(length = 2000)
+    private String descripcion;
+
+    @Lob
+    private byte[] imagen;
+
+    @Column(nullable = false)
+    @Positive
+    private Double puntuacion;
+
+    @Column(length = 10)
+    private String dificultad;
+
+    @Column(length = 10)
+    private String duracion;
+
+    @Column(length = 10)
+    private String comensales;
+
+    @Column(length = 10)
+    private String kcl;
+
+    @Column(length = 2000)
+    private String ingredientes;
+
+    @Column(length = 2000)
+    private String preparacion;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria.id", nullable = false)
+    private Categoria categoria;
+
+    public Receta() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public byte[] getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(byte[] imagen) {
+        this.imagen = imagen;
+    }
+
+    public Double getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(Double puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public String getDificultad() {
+        return dificultad;
+    }
+
+    public void setDificultad(String dificultad) {
+        this.dificultad = dificultad;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getComensales() {
+        return comensales;
+    }
+
+    public void setComensales(String comensales) {
+        this.comensales = comensales;
+    }
+
+    public String getKcl() {
+        return kcl;
+    }
+
+    public void setKcl(String kcl) {
+        this.kcl = kcl;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public String getPreparacion() {
+        return preparacion;
+    }
+
+    public void setPreparacion(String preparacion) {
+        this.preparacion = preparacion;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
